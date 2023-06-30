b2fe509de1a0f2a66b4102cfcae8a45e5fb3f3db
Jesus Marenco <jesusmarencodev@gmail.com>
2023-06-30 11:20:55 -0500

    recordando

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <h3>Counter {{ counter }}</h3>
+
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="reset()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `,
+})
+export class CounterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  counter = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  reset(): void {
+    this.counter = 10;
+  }
+}
